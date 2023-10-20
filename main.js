144ecb163c04f751c1d828540bad441b790e6015
Yoon Robin <robinyoonofficial@gmail.com>
2023-10-20 09:52:46 +0900

    Feat: Adding, Deleting, Re-painting Data

--- main.js
+++ main.js
@@ -1,56 +1,91 @@
-let dummy_data = [
-  {
-    id: 0,
-    value: 75,
-  },
-  {
-    id: 1,
-    value: 20,
-  },
-  {
-    id: 2,
-    value: 100,
-  },
-  {
-    id: 3,
-    value: 50,
-  },
-  {
-    id: 4,
-    value: 80,
-  },
-];
+const DATALIST = "dataList";
 
+let dataList = [];
 
-//
-
-
-
-//3. 값 추가
-const addForm = document.querySelector(".addForm");
-const addIdInput = addForm.getAttributeNames("id");
-const addValueInput = addForm.getAttributeNames("value");
+//2. 값 편집의 테이블
 const dataTable = document.querySelector(".dataTable");
 
-addForm.addEventListener("submit", addValue);
+//3. 값 추가 폼
+const addForm = document.querySelector(".addForm");
 
-function addValue(event){
-  event.preventDefault();
-  console.log("addValue 작동!");
- 
-  let tempData = {
-    //id: string, value: number
-    id: event.target.id.value, 
-    value: +event.target.value.value
-  };
-
-  dummy_data.push(tempData);
-  console.log(dummy_data);
-}
-
-// 4. 값 고급 편집
+// 4. 값 고급 편집 박스
 const dataBox = document.querySelector(".dataBox");
 
-let txt = JSON.stringify(dummy_data);
+function createData(event) {
+  event.preventDefault();
 
-dataBox.textContent = `${txt}`;
+  let dataObj = {
+    //id: number, value: number
+    id: +event.target.id.value,
+    value: +event.target.value.value,
+  };
+
+  paintData(dataObj);
+  saveData(dataObj);
+  //input 값 초기화
+  event.target.id.value = "";
+  event.target.value.value = "";
+}
+
+function saveData(dataObj) {
+  dataList.push(dataObj);
+  saveDataList();
+}
+
+function paintData(dataObj) {
+  const tr = document.createElement("tr");
+  const tdId = document.createElement("td");
+  const tdValue = document.createElement("td");
+
+  const delButton = document.createElement("button");
+  delButton.innerText = "삭제하기";
+  delButton.addEventListener("click", deleteData);
+
+  tdId.innerHTML = dataObj.id;
+  tdValue.innerHTML = dataObj.value;
+  tr.setAttribute("id", dataObj.id);
+  tr.appendChild(tdId);
+  tr.appendChild(tdValue);
+  tr.appendChild(delButton);
+  dataTable.appendChild(tr);
+}
+
+function paintDataOn4(text) {
+  dataBox.textContent = `${text}`;
+}
+
+function deleteData(event) {
+  const { target: button } = event;
+  const tr = button.parentNode;
+  dataTable.removeChild(tr);
+  dataList = dataList.filter(
+    (data) => data.id !== Number(tr.getAttribute("id"))
+  );
+
+  saveDataList();
+}
+
+function saveDataList() {
+  let text = JSON.stringify(dataList);
+
+  localStorage.setItem(DATALIST, text);
+  paintDataOn4(text);
+}
+
+function loadDataList() {
+  const loadedDataList = localStorage.getItem(DATALIST);
+  if (loadedDataList !== null) {
+    const parsedDataList = JSON.parse(loadedDataList);
+    for (let dataObj of parsedDataList) {
+      paintData(dataObj);
+      saveData(dataObj);
+    }
+  }
+}
+
+function init() {
+  loadDataList();
+  addForm.addEventListener("submit", createData);
+}
+
+init();
